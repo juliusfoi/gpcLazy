2252bbc206a78c6ea973aff1fa8037962751c0a0
juliusfoi <jul.foi@accountr.eu>
2011-08-10 01:06:17 +0200

    Nearly finalized, not yet tested

--- converter.js
+++ converter.js
@@ -1,6 +1,11 @@
-str = $('profile').val();
-str.replace(/gatherprofil/,"Gatherprofile");
-str.replace(/waypoints/,"Waypoints_Normal");
-str.replace(/townwaypoints/,"Waypoints_Vendor");
-str.replace(/blacklistednodes/,"BadNodes");
-$('converted_profile').val(str);
+$('script.gpcconverter').append('<div id="converter-field" ><textarea rows="20" cols="70" id="profile"></textarea><textarea rows="20" cols="70" id="converted_profile"></textarea></div>;');
+$('#converter-field').append('<button id="convert">Convert</button>');
+$('#convert').live('click', convert);
+function convert(){
+	str = $('#profile').val();
+	str.replace(/gatherprofil/,"Gatherprofile");
+	str.replace(/waypoints/,"Waypoints_Normal");
+	str.replace(/townwaypoints/,"Waypoints_Vendor");
+	str.replace(/blacklistednodes/,"BadNodes");
+	$('#converted_profile').val(str);
+}
