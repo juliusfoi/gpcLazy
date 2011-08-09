22c8efae4d8cab5f315488e97bd8f36d46925861
juliusfoi <jul.foi@accountr.eu>
2011-08-10 01:30:19 +0200

    Finalized, tested!

--- converter.js
+++ converter.js
@@ -1,11 +1,11 @@
-$('script.gpcconverter').append('<div id="converter-field" ><textarea rows="20" cols="70" id="profile"></textarea><textarea rows="20" cols="70" id="converted_profile"></textarea></div>;');
+function convert() {
+    str = $('#profile').val();
+    str = str.replace(/gatherprofil/gi, "Gatherprofile");
+    str = str.replace(/waypoints/gi, "Waypoints_Normal");
+    str = str.replace(/townwaypoints/gi, "Waypoints_Vendor");
+    str = str.replace(/blacklistednodes/gi, "BadNodes");
+    $('#converted_profile').val(str);
+}
+$('#gpcconverter').html('<div id="converter-field" ><textarea rows="20" cols="70" id="profile"></textarea><textarea rows="20" cols="70" id="converted_profile"></textarea></div>;');
 $('#converter-field').append('<button id="convert">Convert</button>');
 $('#convert').live('click', convert);
-function convert(){
-	str = $('#profile').val();
-	str.replace(/gatherprofil/,"Gatherprofile");
-	str.replace(/waypoints/,"Waypoints_Normal");
-	str.replace(/townwaypoints/,"Waypoints_Vendor");
-	str.replace(/blacklistednodes/,"BadNodes");
-	$('#converted_profile').val(str);
-}
